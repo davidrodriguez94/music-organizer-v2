6329b7097b47564f2ec2ab9b04ae97b5b9cc1c7d
David Rodriguez <rodriguezdiezdavid94@gmail.com>
2018-01-16 23:00:49 +0100

    Declarada variable local en listAllFiles

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -88,8 +88,9 @@ public class MusicOrganizer
      */
     public void listAllFiles()
     {
+        int position = 1;
         for(String filename : files){
-            System.out.println(filename);
+            System.out.println(position + ": " + filename);
         }
     }
 }
